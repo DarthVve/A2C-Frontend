be7f12754e263c132f70bab45e3e15b0b622dbd0
DarthVve <oshevire.majoroh@decagon.dev>
2022-09-18 13:25:12 +0100

    forgot-password: setup reconfig 1.0.2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,4 @@
 export { default as Login } from './login/Login';
-<<<<<<< HEAD
 export { default as ForgotPasswordSL } from './forgotPassword/ForgotPasswordSL';
 export { default as ForgotPasswordCM } from './forgotPassword/ForgotPasswordSL';
 export { default as ForgotPasswordUP } from './forgotPassword/ForgotPasswordUP';
-=======
->>>>>>> 887b568bc2cbe0e79c4ca0d94f7ad466880db509

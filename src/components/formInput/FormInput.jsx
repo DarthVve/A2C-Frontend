62921208538230cff97c7ae7634731cf85233ebd
tofarati <oluwatofarati.agbaje@decagon.dev>
2022-09-19 12:57:14 +0100

    Login: Login page, Button, FormInput, Logo Components, Toastify, Cors, Fonts 1.0.0

---
+++ src/components/formInput/FormInput.jsx
@@ -0,0 +1,36 @@
+import { useRef } from "react";
+import "./formInput.scss";
+
+const FormInput = ({ handleChange, label, type='text', name, placeholder, required=false, value, minLength=0, maxLength=524288, setValidity }) => {
+  const field = useRef();
+  
+  const preHandleChange = (e) => {
+    if(field.current.checkValidity()){
+      typeof setValidity === 'function' && setValidity(prev => {
+        return {
+          ...prev,
+          [field.current.name]: true
+        }
+      });
+    } else {
+      typeof setValidity === 'function' && setValidity(prev => {
+        return {
+          ...prev,
+          [field.current.name]: false
+        }
+      });
+    }
+    handleChange(e);
+  }
+
+  return (
+    <fieldset>
+      <label htmlFor={name}>{label}</label>
+      <input type={type} name={name} placeholder={placeholder} 
+      onChange={preHandleChange} value={value} minLength={minLength}
+      maxLength={maxLength} required={required} ref={field}/>
+    </fieldset>
+  )
+}
+
+export default FormInput;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,6 @@
 export { default as NavBar } from './navbar/NavBar';
 export { default as Footer } from './footer/Footer';
+export { default as LogoWithTypeface } from './logoWithTypeface/LogoWithTypeface';
+export { default as Logo } from './logo/Logo';
+export { default as FormInput } from './formInput/FormInput';
+export { default as Button } from './button/Button';

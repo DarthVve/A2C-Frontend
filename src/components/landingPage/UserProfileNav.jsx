f82313b2a7eefdf90c072ae9fab92767fd49c0b3
stanley omeje <stanley.omeje>
2022-10-06 03:17:43 +0100

    added avatar

--- src/components/landingPage/UserProfileNav.jsx
+++ src/components/landingPage/UserProfileNav.jsx
@@ -1,7 +1,6 @@
 import React,{useState} from 'react';
 import styled from 'styled-components';
 import { FiChevronDown } from 'react-icons/fi';
-import account from '../../assets/account.svg';
 import helpCenter from '../../assets/helpCenter.svg';
 import settings from '../../assets/settings.svg';
 import logoutIcon from '../../assets/logoutIcon.svg';
@@ -32,15 +31,17 @@ function UserProfileNav({dashboard, setIsLogin}) {
     }
   }
 
+  const userIcon = "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' data-name='Layer 1' viewBox='0 0 24 24'%3e%3cpath fill='%230092E4' d='M12,2A10,10,0,0,0,4.65,18.76h0a10,10,0,0,0,14.7,0h0A10,10,0,0,0,12,2Zm0,18a8,8,0,0,1-5.55-2.25,6,6,0,0,1,11.1,0A8,8,0,0,1,12,20ZM10,10a2,2,0,1,1,2,2A2,2,0,0,1,10,10Zm8.91,6A8,8,0,0,0,15,12.62a4,4,0,1,0-6,0A8,8,0,0,0,5.09,16,7.92,7.92,0,0,1,4,12a8,8,0,0,1,16,0A7.92,7.92,0,0,1,18.91,16Z'/%3e%3c/svg%3e"
+
   const route = () => {
     navigate(`/userprofile/${id}`)
   };
 
   return (
     <Profile >
-        <img onClick={setShowModal.bind(null, true)} style={{borderRadius:"50%", width:"40px",height:"40px"}} 
-          src= {avatar}
-        alt='Profil Pic'
+        <img onClick={setShowModal.bind(null, true)} style={{borderRadius:"50%", width:"40px",height:"40px" }} 
+          src={avatar || userIcon}
+        alt='Profile Pic'
         className='profile-avatar-img'
         />
         <span>{username}</span>
@@ -48,7 +49,7 @@ function UserProfileNav({dashboard, setIsLogin}) {
           <>
             <FiChevronDown onClick={()=>setShowDropdown(!showDropdown)}/>
             <Dropdown showDropdown={showDropdown}>
-              <Link to={`/dashboard/${id}`}><DropdownItem><img className='dropdown-img' src={avatar} alt="" onClick={setShowModal.bind(null, true)}/><span>Account</span></DropdownItem></Link>
+              <Link to={`/dashboard/${id}`}><DropdownItem><img className='dropdown-img' src={avatar || userIcon} alt="" onClick={setShowModal.bind(null, true)}/><span>Account</span></DropdownItem></Link>
               <DropdownItem><img src={settings} alt='settings' /><span onClick={route}>Settings</span></DropdownItem>
               <DropdownItem><img src={helpCenter} alt='help center' /><span>Help Center</span></DropdownItem>
               <DropdownItem><img src={logoutIcon} alt='logout'/><span onClick={handleLogout}>Logout</span></DropdownItem>
@@ -67,9 +68,9 @@ const Profile = styled.div`
   gap:10px;
   cursor: pointer;
 
-  &.Profile-Avatar-img {
-      width: 36px;
-      border-radius: 50%;
+  &.profile-avatar-img {
+    width: 36px;
+    border-radius: 50%;
   }
 `
 

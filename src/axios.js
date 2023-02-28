fc95f0c511818e802982c0ab3c518f9b59b266f2
Darth Vve <osheviremajoroh@gmail.com>
2023-02-28 22:17:54 +0100

    Backend integaration (Deployed on fly) 1

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
+import Cookies from 'js-cookie'
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
+const token = Cookies.get('token');
 
 export default axios.create({
     baseURL: BASE_URL,
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json", "Authorization": `Bearer ${token}` },
     withCredentials: true
 });
